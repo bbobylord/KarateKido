102ee3c6beab9c1000cf1a91fe1571249d7cc89a
Ashkan Shasadeghi <AshkanSadeghi023.as@gmail.com>
2021-07-06 15:07:39 +0430

    erjh
    cvh

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,14 +1,28 @@
 let Time = Number(10000);
 let ReadyTime = Number(3);
+let Day = "https://s19.picofile.com/file/8437587650/Day.png"
+let Night = "https://s18.picofile.com/file/8437587668/Night.png"
+var d = new Date();
+var h = d.getHours();
+
+function ChangeBackground(Hour) {
+    if(Hour < 19 && Hour > 7){
+        document.getElementById("Main").style.background = `url("${Day}") center center / cover no-repeat`;
+    } else {
+        document.getElementById("Main").style.background = `url("${Night}") center center / cover no-repeat`;
+    }
+}
 
 
 setInterval(() => {
     
     Start();
+    ChangeBackground(h);
 
 }, 1000);
 
 function Start() {
+
     if(ReadyTime !== 1){
         ReadyTime --;
         document.getElementById("Ready").innerHTML = ReadyTime;
@@ -66,6 +80,14 @@ function Strick(Float) {
 
     document.getElementById("Record").innerHTML = Record + 10;
 
+    CheckCharacter(Float);
+
     console.log(Float);
 
+}
+
+function CheckCharacter(Float) {
+    document.querySelector("#Left").classList.add("Disable");
+    document.querySelector("#Right").classList.add("Disable");
+    document.querySelector("#" + Float).classList.remove("Disable");
 }
